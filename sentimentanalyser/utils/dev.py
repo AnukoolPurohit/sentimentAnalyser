56bbbc66084a29583fe311ada1db733f8686a140
Anukool Purohit <anukoolpurohit@gmail.com>
2020-08-19 19:33:48 +0530

    My Experiments with Hooks

--- sentimentanalyser/utils/dev.py
+++ sentimentanalyser/utils/dev.py
@@ -1,7 +1,2 @@
-def get_info(x, pad_id=1):
-    mask = (x == pad_id)
-    lenghts = x.size(1) - (x == pad_id).sum(1)
-    return lenghts, mask
-
 def print_dims(name, tensor):
     print(f'size of {name} is {tensor.shape}')
